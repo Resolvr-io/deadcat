29e2f846c524df01f65df18c41fa5a593c4d828b
The Daniel <dmnyc@users.noreply.github.com>
2026-03-27 12:11:23 -0400

    fix: remove needless borrows in chain_adapter

--- src-tauri/src/chain_adapter.rs
+++ src-tauri/src/chain_adapter.rs
@@ -53,7 +53,7 @@ impl ElectrumChainAdapter {
     ) -> Result<Option<(u32, [u8; 32])>, ChainAdapterError> {
         let client = self.client()?;
 
-        let height = match get_tx_confirmed_height(&client, self, txid)? {
+        let height = match get_tx_confirmed_height(client, self, txid)? {
             Some(h) => h,
             None => return Ok(None),
         };
@@ -67,7 +67,7 @@ impl ElectrumChainAdapter {
             return Ok(None);
         }
 
-        let block_hash = get_block_hash(&client, height)?;
+        let block_hash = get_block_hash(client, height)?;
         Ok(Some((height, block_hash)))
     }
 }
@@ -406,7 +406,7 @@ impl ChainSource for ElectrumChainAdapter {
     fn get_transaction(&self, txid: &[u8; 32]) -> Result<Option<Vec<u8>>, Self::Error> {
         let client = self.client()?;
         let txid_hex = txid_to_display_hex(txid);
-        let Some(resp) = transaction_get_response(&client, &txid_hex, false)? else {
+        let Some(resp) = transaction_get_response(client, &txid_hex, false)? else {
             return Ok(None);
         };
         let hex_str = resp
